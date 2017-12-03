235ff9c9108fea8e288695b1a471acce675eb53b
effects67 <32003168+effects67@users.noreply.github.com>
2017-12-03 12:00:24 -0600

    Adding 1.5.js

---
+++ week-1/Vance-1.5.js
@@ -0,0 +1,84 @@
+var myFirstName = "Gabriel";
+var myLastName = "Vance";
+var todaysDate = new Date().toLocaleDateString();
+var assignmentNum = "Assignment 1.5";
+
+var programHeader = "\n" + myFirstName + " " + myLastName + "\n"
+    + assignmentNum + "\nDate: " + todaysDate;
+
+console.log(programHeader);
+console.log("\n");
+
+var firstName0 = "First Name: Bob";
+var lastName0 = "Last Name: Adams";
+var address0 = "Address: 123 Rainbow Rd Omaha, NE";
+var payRate0 = "Pay Rate: $20.00";
+var hireDate0 = new Date(2014, 5, 1)
+
+
+var employee0 = "\n" + firstName0 + "\n" + lastName0 + "\n"
+    + address0 + "\n" + payRate0+ "\nDate: " + hireDate0;
+
+console.log(employee0);
+console.log("\n");
+
+var firstName1 = "First Name: Karryn";
+var lastName1 = "Last Name: Stone";
+var address1= "Address: 123 Alpha Dr Omaha, NE";
+var payRate1 = "Pay Rate: $60.00";
+var hireDate1 = new Date(2012, 1, 9)
+
+
+var employee1 = "\n" + firstName1 + "\n" + lastName1 + "\n"
+    + address1 + "\n" + payRate1+ "\nDate: " + hireDate1;
+
+console.log(employee1);
+console.log("\n");
+
+var firstName2 = "First Name: Mike";
+var lastName2 = "Last Name: McDonald";
+var address2 = "Address: 621 Farmer's Rd Omaha, NE";
+var payRate2 = "Pay Rate: $10.00";
+var hireDate2 = new Date(2017, 2, 6)
+
+
+var employee2 = "\n" + firstName2 + "\n" + lastName2 + "\n"
+    + address2 + "\n" + payRate2+ "\nDate: " + hireDate2;
+
+console.log(employee2);
+console.log("\n");
+
+var firstName3 = "First Name: Tommy";
+var lastName3 = "Last Name: Tanker";
+var address3 = "Address: 123 Railroad St Omaha, NE";
+var payRate3 = "Pay Rate: $70.50";
+var hireDate3 = new Date(2014, 4, 3)
+
+
+var employee3 = "\n" + firstName3 + "\n" + lastName3 + "\n"
+    + address3 + "\n" + payRate3 + "\nDate: " + hireDate3;
+
+console.log(employee3);
+console.log("\n");
+
+var firstName4 = "First Name: Robert";
+var lastName4 = "Last Name: Gonzales";
+var address4 = "Address: 555 Harlan Dr Omaha, NE";
+var payRate4 = "Pay Rate: $19.20";
+var hireDate4 = new Date(2016, 11, 7)
+
+
+var employee4 = "\n" + firstName4 + "\n" + lastName4 + "\n"
+    + address4 + "\n" + payRate4 + "\nDate: " + hireDate4;
+
+console.log(employee4);
+console.log("\n");
+
+
+
+
+
+
+
+
+
