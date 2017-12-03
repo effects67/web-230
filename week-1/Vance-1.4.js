c2b2589d4aa9f27949c1d9a5040e4b808b284090
effects67 <32003168+effects67@users.noreply.github.com>
2017-12-02 19:24:18 -0600

    Adding 1.3.js and 1.4.js

---
+++ week-1/Vance-1.4.js
@@ -0,0 +1,33 @@
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+var firstName="Gabriel";
+var lastName="Vance";
+var age="24";
+var street="Harlan";
+var city="Bellevue";
+var state="Nebraska";
+var zipCode="68005";
+
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(state);
+console.log(zipCode);
+
+
+
