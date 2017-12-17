f0efd7aa4cd02dfd6f26c0627f9f33ad4dd486c5
effects67 <32003168+effects67@users.noreply.github.com>
2017-12-17 17:29:18 -0600

    adding 3.2, 3.3, 3.4

---
+++ week-3/Vance-3.3.js
@@ -0,0 +1,62 @@
+var header = require('../header.js'); 
+
+// start program
+
+/*
+    Expected output:
+
+    FirstName LastName
+    Exercise 3.3
+    Today's Date
+
+    // Expected output
+    The enter key was pressed 
+
+*/ 
+
+// Multiway branch of if statements (replace with a switch statement)
+
+/*
+
+var eventKeyCode = 13;
+
+if (eventKeyCode == 13) {
+    console.log("The enter key was pressed"); 
+} else if (eventKeyCode == 16) {
+    console.log("The shift key was pressed"); 
+} else if (eventKeyCode == 32) {
+    console.log("The spacebar key was pressed")
+} else if (eventKeyCode == 8) {
+    console.log("The backspace / delete key was pressed");
+} else {
+    console.log("Unrecognized key press"); 
+}
+*/
+console.log(header.display("Gabriel", "Vance", "Exercise 3.3")); 
+
+
+
+
+var eventKeyCode = "enter";
+
+switch (eventKeyCode) {
+
+    case "delete":
+        console.log("The delete key was pressed");
+        break;
+    case "space":
+        console.log("The space key was pressed");
+        break;
+    case "enter":
+        console.log("The enter key was pressed");
+        break;
+    case "tab":
+        console.log("The tab key was pressed");
+        break;
+    default:
+        console.log("Invalid Choice");
+        break;
+}
+
+
+// end program 
---
+++ week-3/vance-3.2.js
@@ -0,0 +1,89 @@
+
+
+
+var header = require('../header.js'); 
+
+// start program
+
+/*
+    Expected output:
+
+    FirstName LastName
+    Exercise 3.2
+    Today's Date
+
+    // output from the match() function 
+    false
+    true 
+
+    // output from the if...else blocks 
+    Truck and Car do not match!
+    Bike and Bike do match!
+    Four and Three do not match!
+*/ 
+
+
+
+// functions 
+function match(ex1,ex2) {
+	if (ex1!==ex2){
+	console.log(false);
+}
+	else {
+		console.log(true);
+} 
+
+function logMismatch(ex1,ex2) {
+	console.log(ex1 + " " + "and" + " " + ex2 + " " + "do not match!");
+}
+
+function logMatch(ex1,ex2) {
+	console.log(ex1 + " " + "and" + " " + ex2 + " " + "do match!");
+}
+
+// six (6) test variables 
+
+var t="Truck";
+var c="Car";
+var b="Bike";
+var bb="Bike"
+var th="Three"
+var f="Four";
+
+
+
+// Output from the match() function... 
+console.log(header.display("Gabriel", "Vance", "Exercise 3.2")); 
+console.log(match("A", "B"));
+console.log(match(2, 2)); 
+
+
+// Conditional "if...else" statements. Include checks for all six (6) test variables 
+
+
+if (t=c) {
+    logMatch(t,c);
+} else {
+    logMismatch(t,c);
+}
+
+
+
+
+if (b=bb) {
+    logMatch(b,bb);
+} else {
+    logMismatch(b,bb);
+}
+
+
+
+
+if (th=f) {
+    logMatch(th,f);
+} else {
+    logMismatch(th,f);
+}
+
+
+
